e45a2e53fd82e95deea56b836e5346e65d02dd06
Philip Yu <pyu3@mail.sfsu.edu>
2019-08-12 00:04:37 -0700

    Add multi-screen navigation

---
+++ lib/results_page.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+class ResultsPage extends StatefulWidget {
+  @override
+  _ResultsPageState createState() => _ResultsPageState();
+}
+
+class _ResultsPageState extends State<ResultsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(
+          'BMI Calcuilator',
+        ),
+      ),
+      body: Text(
+        'Hello',
+      ),
+    );
+  }
+}
